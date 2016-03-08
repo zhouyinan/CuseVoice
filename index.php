44b3300b1e7e9d3b2bded6cd202ab29a7a7f9877
Yinan Zhou <zhouyinan@outlook.com>
2016-03-08 11:12:37 -0500

    Add switch for final round

--- index.php
+++ index.php
@@ -3,4 +3,8 @@ define('APP_DEBUG', true);
 define('BUILD_DIR_SECURE', false);
 define('APP_PATH', './Application/');
 define('RUNTIME_PATH','./Runtime/');
+
+//If it is final round, uncomment the following line
+//define('BIND_MODULE','Admin');
+
 require './ThinkPHP/ThinkPHP.php';
