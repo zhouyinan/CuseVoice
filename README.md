ee049345c93abdc3b0dd7b67d562dc91d6f94653
Yinan Zhou <zhouyinan@outlook.com>
2016-03-07 23:32:34 -0500

    Add SQL file for creating tables in MySQL database

--- README.md
+++ README.md
@@ -3,13 +3,20 @@
 这是一个因雪城大学华人学生学者联合会举办雪城好声音大赛要求而编写的报名、评分系统，使用了ThinkPHP框架。
 
 ## 运行环境
-* PHP
-* MySQL
+* PHP（与ThinkPHP框架要求相同：PHP5.3以上版本，不支持PHP5.3dev版本和PHP6）
+* MySQL（需支持InnoDB引擎）
 
 请特别注意文件权限，运行时ThinkPHP框架会自动创建Runtime目录，可能需要写权限
 
-## 配置
+## 安装
+
+### 配置
 
 配置文件位于 Application/Common/Conf/config.php
 
 请根据文件内的注释进行配置
+
+### 建立数据表
+
+1. 在MySQL内建立以InnoDB为引擎的数据库
+2. 在该数据中运行install.sql
---
+++ install.sql
@@ -0,0 +1,46 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+DROP TABLE IF EXISTS `contestants`;
+CREATE TABLE `contestants` (
+  `netid` char(28) COLLATE utf8_bin NOT NULL COMMENT 'NETID',
+  `name` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '姓名',
+  `firstname` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT 'First Name',
+  `lastname` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT 'Last Name',
+  `mobile` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT 'Mobile',
+  `song` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT 'Song',
+  `note` varchar(512) COLLATE utf8_bin NOT NULL,
+  `accompany_uploaded` bit(1) NOT NULL DEFAULT b'0',
+  `confirmed` bit(1) NOT NULL DEFAULT b'0',
+  `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`netid`),
+  KEY `name` (`name`),
+  KEY `firstname` (`firstname`),
+  KEY `lastname` (`lastname`),
+  KEY `name_2` (`name`),
+  KEY `name_3` (`name`),
+  KEY `firstname_2` (`firstname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Information of contestants of Syracuse Voice';
+
+DROP TABLE IF EXISTS `graders`;
+CREATE TABLE `graders` (
+  `grader_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `grader_name` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`grader_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS `grades`;
+CREATE TABLE `grades` (
+  `grade_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `contestant_netid` char(28) COLLATE utf8_bin DEFAULT NULL,
+  `grader` tinyint(3) unsigned DEFAULT NULL,
+  `score` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`grade_id`),
+  KEY `contestant_netid` (`contestant_netid`,`grader`),
+  KEY `grader` (`grader`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+ALTER TABLE `grades`
+  ADD CONSTRAINT `contestant_consistency` FOREIGN KEY (`contestant_netid`) REFERENCES `contestants` (`netid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `grader_consistency` FOREIGN KEY (`grader`) REFERENCES `graders` (`grader_id`) ON DELETE CASCADE ON UPDATE CASCADE;
