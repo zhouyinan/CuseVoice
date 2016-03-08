299c969c362fd964dd53f50b67156502a7adc720
Yinan Zhou <zhouyinan@outlook.com>
2016-03-08 03:08:13 -0500

    Contestant Dashboard Template Change
    change to use extension of layout.html

--- Application/Contestant/Lang/zh-cn.php
+++ Application/Contestant/Lang/zh-cn.php
@@ -3,4 +3,5 @@ return array(
   'CONTESTANT_LOGIN_TITLE' => '登录赛事选手平台',
   'CONTESTANT_LOGIN_HELP' => '请输入您的NETID以登录系统',
   'CONTESTANT_LOGIN_CHECK_BUTTON' => '登录 / 注册'
+  'VIEW_REGISTRATION_TITLE' => '查看报名状态',
 );
