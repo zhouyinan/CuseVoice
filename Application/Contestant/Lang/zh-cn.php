687df94586674ee5cd2683b7a48e3ddf9a22e077
Yinan Zhou <zhouyinan@outlook.com>
2016-03-08 18:06:31 -0500

    Fix bugs
    syntax error in zh-cn.php
    add recaptcha JS library to admin login page
    change link on home page

--- Application/Contestant/Lang/zh-cn.php
+++ Application/Contestant/Lang/zh-cn.php
@@ -2,7 +2,7 @@
 return array(
   'CONTESTANT_LOGIN_TITLE' => '登录赛事选手平台',
   'CONTESTANT_LOGIN_HELP' => '请输入您的NETID以登录系统',
-  'CONTESTANT_LOGIN_CHECK_BUTTON' => '登录 / 注册'
+  'CONTESTANT_LOGIN_CHECK_BUTTON' => '登录 / 注册',
   'VIEW_REGISTRATION_TITLE' => '查看报名状态',
   'STATUS_UNCONFIRMED' => '成功提交报名表<br />等待SUCSSA确认',
   'STATUS_CONFIRMED' => 'SUCSSA 已确认',
