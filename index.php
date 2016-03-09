f046007595fc8226bf28f9fb520b07a1509dbfc6
Yinan Zhou <zhouyinan@outlook.com>
2016-03-08 19:58:21 -0500

    remove test configuration

--- index.php
+++ index.php
@@ -3,5 +3,4 @@ define('APP_DEBUG', true);
 define('BUILD_DIR_SECURE', false);
 define('APP_PATH', './Application/');
 define('RUNTIME_PATH','./Runtime/');
-define('APP_STATUS','local');
 require './ThinkPHP/ThinkPHP.php';
